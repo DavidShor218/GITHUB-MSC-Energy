83b3d935459638714a7f54cf91debb53a59ffd10
DavidShor218 <davidshor96@outlook.com>
2024-10-29 15:22:15 +0000

    Update 241029_PV_Exercise_Day1.py

--- 241029_PV_Exercise_Day1.py
+++ 241029_PV_Exercise_Day1.py
@@ -11,12 +11,16 @@ def solar_PV_function(building_width,building_length,roof_angle,pv_width,pv_heig
     solarpv_area = pv_width/1000*pv_height/1000
     Num_panels = math.floor(roof_area/solarpv_area)
     Total_kWp = pv_power*Num_panels/1000
-    #print("For a building length {:.1f} m and width {:.1f} m, \ with a roof with inclined with an angle {}, you can fit {} Solar PV panels \of {:.2f} kWp. You can install a total of {} kWp".format(building_length,building_width,)
-        
+    
     print(Num_panels)
     print(Total_kWp)
     print(roof_length)
     print(roof_area)
     
+    return Num_panels, Total_kWp, roof_area
     
-solar_PV_function(8,28,22,1690,1046,400)
+numpanels, power, available_area = solar_PV_function(8,28,22,1690,1046,400)
+
+print("Total panels:",numpanels)
+print("Total installed Power: {:.1f} kWp and roof area: {:.2f} m2".format(power,available_area))
+
